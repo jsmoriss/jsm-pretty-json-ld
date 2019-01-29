98bac3edfc6d142cc88c19093805d42371f4b89f
JS Morisset <jsm@surniaulula.com>
2019-01-29 08:55:48 -0600

    v1.0.1

--- jsm-pretty-json-ld.php
+++ jsm-pretty-json-ld.php
@@ -11,7 +11,7 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Re-format the compressed LD+JSON / JSON-LD output from Yoast SEO and other plugins to create human readable (aka "pretty") code.
- * Requires PHP: 5.4
+ * Requires PHP: 5.5
  * Requires At Least: 3.8
  * Tested Up To: 5.0
  * Version: 1.0.1
