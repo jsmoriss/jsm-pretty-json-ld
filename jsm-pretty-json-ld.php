f8863fadeb76460f377887d99101fe6b15868c16
JS Morisset <jsm@surniaulula.com>
2020-09-15 13:39:13 -0700

    v1.1.0

--- jsm-pretty-json-ld.php
+++ jsm-pretty-json-ld.php
@@ -13,7 +13,7 @@
  * Description: Re-format Schema LD+JSON / JSON-LD from Yoast SEO, WooCommerce, and others to create human readable (aka "pretty") code.
  * Requires PHP: 5.6
  * Requires At Least: 4.2
- * Tested Up To: 5.5
+ * Tested Up To: 5.5.1
  * WC Tested Up To: 4.5.2
  * Version: 1.1.0
  * 
