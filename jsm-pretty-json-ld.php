83ae5f1d69bf23bc850b9dbf7ab853745bd23399
JS Morisset <jsm@surniaulula.com>
2021-09-24 06:46:28 -0700

    v1.2.0

--- jsm-pretty-json-ld.php
+++ jsm-pretty-json-ld.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.0
  * Requires At Least: 5.0
  * Tested Up To: 5.8.1
- * WC Tested Up To: 5.7.0
+ * WC Tested Up To: 5.7.1
  * Version: 1.2.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
