492bcaf3c358126019c72e27c941a12982634cab
jsmoriss <jsm@surniaulula.com>
2026-07-20 04:17:30 +0000

    v1.2.0

--- jsm-pretty-json-ld.php
+++ jsm-pretty-json-ld.php
@@ -13,7 +13,7 @@
  * Description: Re-format Schema LD+JSON / JSON-LD from Yoast SEO, WooCommerce, and others to create human readable (aka "pretty") code.
  * Requires PHP: 7.4.33
  * Requires At Least: 6.0
- * Tested Up To: 7.0
+ * Tested Up To: 7.0.2
  * WC Tested Up To: 10.8.1
  * Version: 1.2.0
  *
